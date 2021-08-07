ca0e9c2f1237cbe239fa61ca7c7ffaad22232044
Ольга Перерва <olga.pererva+qa@forforce.com>
2021-08-07 10:37:13 +0300

    'newrepositorii'

---
+++ main.js
@@ -0,0 +1,45 @@
+function buttonClick(){
+    // let isAdmin = confirm('Вы администратор?');
+    // console.log(isAdmin);
+    //
+    // let current = prompt('Какой сейчас год?', 2021);
+    // let age = prompt('Год рождения?');
+    // const res = current - age;
+    // alert('Ваш возраст '+res);
+
+    // let test1 = 123;
+    // let test2 = "string";
+    // console.log(String(test1), typeof String(test2));
+    // console.log(Number(test1), typeof Number(test2));
+
+    // const year = prompt("Введите год");
+    //
+    // if (year == 2021){
+    //     alert("Ura!");
+    // } else if (year == 2022){
+    //     alert("Da!");
+    // } else{
+    //     alert("Oi!");
+    // }
+
+    // const year = 2021;
+    // switch (year){
+    //     case 2021:
+    //         alert('2021');
+    //         break;
+    //     case 2022:
+    //         alert('2021');
+    //         break;
+    //     case 2023:
+    //         alert('2021');
+    //         break;
+    //     default://else
+    //         alert('oi');
+    // }
+
+    const year = prompt("god");
+    const test = year === "2021" ? "Yes" : "No";
+    alert(test);
+
+}
+
