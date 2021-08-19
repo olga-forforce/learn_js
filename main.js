68cc6a548f6fd065e0e7ab10096197c811554f6a
Ольга Перерва <olga.pererva+qa@forforce.com>
2021-08-19 09:37:32 +0300

    'HW12.08'

--- main.js
+++ main.js
@@ -61,38 +61,3 @@ function Num() {
     }
 }
 
-    // let isAdmin = confirm('Вы администратор?');
-    // console.log(isAdmin);
-    //
-    // let current = prompt('Какой сейчас год?', 2021);
-    // let age = prompt('Год рождения?');
-    // const res = current - age;
-    // alert('Ваш возраст '+res);
-
-    // let test1 = 123;
-    // let test2 = "string";
-    // console.log(String(test1), typeof String(test2));
-    // console.log(Number(test1), typeof Number(test2));
-
-    //
-
-    // const year = 2021;
-    // switch (year){
-    //     case 2021:
-    //         alert('2021');
-    //         break;
-    //     case 2022:
-    //         alert('2021');
-    //         break;
-    //     case 2023:
-    //         alert('2021');
-    //         break;
-    //     default://else
-    //         alert('oi');
-    // }
-
-    // const year = prompt("god");
-    // const test = year === "2021" ? "Yes" : "No";
-    // alert(test);
-
-
